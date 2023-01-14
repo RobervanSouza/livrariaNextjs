830aa4728b6fb94158bc3cc92483ac2908f15ded
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-01-14 12:01:56 -0300

    criando context

--- livro/pages/components/context/context.js
+++ livro/pages/components/context/context.js
@@ -3,7 +3,21 @@ import { createContext, useState } from "react";
 const ContexFavotiro = createContext(null)
 
 const ContextProvider = ({children}) => {
-        const [favorito, setFavotiro] = useState([])
+        const [favoritos, setFavotiro] = useState([])
   
+        const addFavoritos = () =>{
 
+        }
+        
+        const removeFavoritos = (id) =>{
+
+
+        }
+        
+    return(
+        <ContexFavotiro.Provider value={{ favoritos, addFavoritos, removeFavoritos }}  > 
+            {children}
+        </ContexFavotiro.Provider>
+
+    )
 }
